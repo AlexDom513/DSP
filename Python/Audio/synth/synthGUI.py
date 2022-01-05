94fdacb117b95b55635cbf73af0e88065a0a88ca
AlexDom513 <57962269+AlexDom513@users.noreply.github.com>
2022-01-04 19:22:42 -0600

    graph widget implementation

--- Python/Audio/synth/synthGUI.py
+++ Python/Audio/synth/synthGUI.py
@@ -1,50 +1,77 @@
+#useful links/resources:
 #https://stackoverflow.com/questions/17466561/best-way-to-structure-a-tkinter-application
 #https://pythonprogramming.net/tkinter-depth-tutorial-making-actual-program/
 
-#note: perhaps different sections of the GUI could be formatted into a class, then the overall components could be rearranged, this
-#can be addressed in a later version of the synthesizer
 import Tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 from synthRunner import runner
-from time import sleep
 
-#order: first we must have all of our functionality and processing, only then should we move forward with GUI design
-
-#local variables
-volume = 1     # range [0.0, 1.0]
-fs = 44100       # sampling rate, Hz, must be integer
+fs = 44100       # sampling rate, Hz, must be integer, need 44100 samples for 1 second of audio
 freq = 440.0     # sine frequency, Hz, may be float
-duration = 2   # in seconds, may be float
+duration = 1   # in seconds, may be float
 
 #create the sound player object
-run = runner(volume, fs, freq, duration)
+run = runner(fs, freq, duration)
+
+def plot():
+    global a
+    global canvas
+    a.clear()
+    a.plot(run.disp[:300])
+    canvas.draw()
 
 #idea is that we would first instantiate any necessary processing objects and then call their specific commands from the button
 def testCallBack():
     freq = freqSlider.get()
     run.convertFreq(freq)
-    run.generateSamples()
+    run.sine()
+    plot()
     run.stream()
-#need 44100 samples for 1 second of audio
 
-
-#idea instantiate all the basic tkinter objects and processing objects, execute, idea we could actually have a bunch of buttons for the different frequencies
-#maybe ill do separate versions of this project
+#tkinter setup
 master = tk.Tk()
+master.title('PySynth v1')
+master.iconbitmap('icon (1).ico')
+actionFrame = tk.Canvas(master)
+buttonFrame = tk.Canvas(master)
+graphFrame = tk.Canvas(master)
 
-#we will use a slider to set the frequency value
-freqSlider = tk.Scale(master, from_=400, to=1000, orient=tk.HORIZONTAL)
-freqSlider.pack()
+#canvas (group similar widgets together)
+actionFrame.pack(side='left', expand=True)
+graphFrame.pack(expand=False)
+buttonFrame.pack(side='right',expand=True)
 
-toggleButton = tk.Button(master, text='toggle sound', command=testCallBack)
-toggleButton.pack()
+#graphFrame
+f = Figure(figsize=(3,2), dpi=100)
+a = f.add_subplot(111)
+a.plot([])
+canvas = FigureCanvasTkAgg(f, master=graphFrame)
+canvas.draw()
+canvas.get_tk_widget().pack()
 
-sineButton = tk.Button(master, text='Sine')
-sineButton.pack()
-sawButton = tk.Button(master, text='Saw')
-sawButton.pack()
-triangleButton = tk.Button(master, text='Triangle')
-triangleButton.pack()
-noiseButton = tk.Button(master, text = 'Noise')
-noiseButton.pack()
+#actionFrame
+freqSlider = tk.Scale(actionFrame, from_=100, to=1000, orient=tk.HORIZONTAL)
+freqSlider.grid(column=0,row=0)
+toggleButton = tk.Button(actionFrame, text='Play!', command=testCallBack)
+toggleButton.grid(column=0, row=1)
+
+#buttonFrame
+padX = 0
+padY = 0
+buttonWidth = 7
+
+sineButton = tk.Button(buttonFrame, text='Sine', width=buttonWidth)
+sineButton.grid(column=0, row=2, padx=padX, pady=padY)
+
+sawButton = tk.Button(buttonFrame, text='Saw', width=buttonWidth)
+sawButton.grid(column=1, row=2, padx=padX, pady=padY)
+
+triangleButton = tk.Button(buttonFrame, text='Triangle', width=buttonWidth)
+triangleButton.grid(column=0, row=3, padx=padX, pady=padY)
+
+noiseButton = tk.Button(buttonFrame, text = 'Noise', width=buttonWidth)
+noiseButton.grid(column=1, row=3, padx=padX, pady=padY)
 
 master.mainloop()
--- Python/Audio/synth/synthRunner.py
+++ Python/Audio/synth/synthRunner.py
@@ -1,41 +1,67 @@
 #goal: develop a program(simulator) where we can test different waveforms and implement different types of filtering/algorithms
+#refer to prototyping file for explanantion of how the samples are generated
 
-#useful links/resources
+#useful links/resources:
 #https://stackoverflow.com/questions/8299303/generating-sine-wave-sound-in-python (notice 2nd response: include .tobytes())
 
 import pyaudio
 import numpy as np
 
 class runner:
-    def __init__(self, volume, fs, freq, duration):
-        self.volume = volume
+    def __init__(self, fs, freq, duration):
         self.fs = fs
         self.freq = freq
+        self.duration = duration
+        self.disp = []
         self.numSamples = None
         self.samples = None
-        self.duration = duration
+        self.repeat = None
         self.player = pyaudio.PyAudio()
 
     def convertFreq(self, freq):
         self.freq = freq
         self.numSamples = np.round(self.fs / self.freq)
+        self.repeat = np.ceil((1 + self.fs / self.numSamples) * self.duration)
 
-    def generateSamples(self):
+    def sine(self):
         num = self.numSamples
         n = np.linspace(0,num,num = num)
         x = np.sin(2*np.pi*n)
         x = x[:-1]
-        repeat = np.ceil((1 + self.fs / num) * self.duration)
-        samples = np.tile(x,repeat)
+        samples = np.tile(x,self.repeat)
+        self.disp = samples
+        self.samples = samples.astype(np.float32).tobytes()
 
+    def saw(self):
+        num = self.numSamples
+        n = np.linspace(0,num,num = num)
+        n = n[:-1]
+        samples = np.tile(n,self.repeat)
+        self.disp = samples
+        self.samples = samples.astype(np.float32).tobytes()
 
-        print(len(samples))
-        print(1.0* len(samples) / (self.fs))
+    def triangle(self):
+        num = self.numSamples
+        numHalf = round(num/2)
+        n1 = np.linspace(0,numHalf,num = numHalf)
+        n2 = np.linspace(numHalf,2*numHalf,num = numHalf)
+        n2 = n2[::-1]
+        n2 = n2-numHalf
+        n = np.concatenate((n1, n2), axis=0)
+        n = n[:-1]
+        samples = np.tile(n,self.repeat)
+        self.disp = samples
+        self.samples = samples.astype(np.float32).tobytes()
 
+    def noise(self):
+        num = self.numSamples
+        x = np.random.randn(num)
+        x = x[:-1]
+        samples = np.tile(x,self.repeat)
+        self.disp = samples
         self.samples = samples.astype(np.float32).tobytes()
 
     def stream(self):
-
         stream = self.player.open(format=pyaudio.paFloat32,
                 channels=1,
                 rate=self.fs,
